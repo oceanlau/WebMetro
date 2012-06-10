7d134b4cb74ca68566bcb8ec53412ea102229e46
oceanlau <liuy.ocean@gmail.com>
2012-06-11 01:34:29 +0800

    All effects ready

--- README.md
+++ README.md
@@ -2,3 +2,92 @@ Web Metro
 =========
 
 A Windows Phone Metro styled site. Our school blocks github ssh. Please check back later.
+
+An early preview: [http://oceanlau.github.com/WebMetro/](http://oceanlau.github.com/WebMetro/)
+
+Effect
+======
+
+Interaction Effects
+-------------------
+
+###Effect: Press###
+
+###Effect: Turn###
+
+Presentation Effects
+--------------------
+
+Now every tile could only has one and one only Presentation effect trigger. It's sad. Multiple trigger will be the very next feature I'll be working on, which requires a lot of code rewrite. Now, however if you want to use "hover" and "auto" at the same time you can always set your own rule. Just set `p.manual` of the tile to `false` . It will keep Automaton from manipulate your tile while your mouse is on it. Set it to `true` when you've done your animation.
+
+###Effect: Backface###
+
+**Setting Attr:**
+
+- `data-trigger`: "auto", "hover"
+
+**Struct:**
+
+`div.backface > div.card > div.face + div.face.back.flip-x` or `div.backface > div.card > div.face + div.face.back.flip-y`
+
+**Note:**
+
+- The `.flip-x` or `.flip-y` class determines your card's flip direction.
+
+###Effect: Extend###
+
+**Setting Attr:**
+
+- `data-trigger`: "auto", "hover"
+- `data-dir`
+
+**Struct:**
+
+if `data-dir` === "top" || `data-dir` === "bottom"
+
+`div.metro-content.extend > div.extender + div.extendee`
+
+else if `data-dir` === "left" || `data-dir` === "right"
+
+`div.metro-content.extend > div > div.extender+ div.extendee`
+
+**Note:**
+
+- `div.extender` should always be height:100%; to the `div.metro-content`
+
+###Effect: Shatter###
+
+**Setting Attr:**
+
+ - `data-trigger`: "auto", "hover", "free"
+
+**Struct:**
+
+`div.metro-content.shatter > div.backface.fragment > ...(Backface)`
+
+**Note:**
+
+- Let's say you have four pieces of `div.fragment`, you need to use CSS to manually set your `div.fragment` width:50%;height:50%;. Read the CSS source.
+- Setting `data-trigger` to "free" means putting the behavior of 'div.fragment' under the full control of Automaton.
+- The `data-trigger` you set on Backface tile who also has a `.fragment` class will be ignored. Thus I can put these fragments' trigger sys under the control of `div.shatter`.
+
+###Effect: Tag###
+
+**Setting Attr:**
+
+- `data-trigger`: "auto", "hover"
+- `data-dir`
+
+**Struct:**
+
+`div.metro-content.tag > div.sign`
+
+**Note:**
+
+- Let's say you set `data-dir` to "top", you need to use CSS to manually set your `div.sign` off the top of your `div.tag`. Read the CSS source.
+
+Planned
+-------
+
+- Multiple trigger for one tile.
+- Enhanced Shatter effect. Fragments dispatched at a different delay.
--- Scripts/metro-css3.js
+++ Scripts/metro-css3.js
@@ -1,151 +1,374 @@
-//Forked from olton / Metro-UI-CSS
-//original: https://github.com/olton/Metro-UI-CSS/blob/master/demo/js/metro.js
+;(function($, window, undefined){
+    $.fn.metro = function(options){
 
-;(function($){
-    $.fn.metro = function(){
+        var opts = $.extend({}, $.fn.metro.defaults, options);
+        var _el = opts.el;
+        var _page = {
+            scrollLeft: document.body.scrollLeft 
+        }
 
-        var _el = {
-                body:'metro-body',
-                section:'metro-section',
-                content:'metro-content'
-            }
+        //Turn Interaction Effect - init
+        //$('.' + _el.top).css({ '-webkit-perspective' : $('.' + _el.top).outerHeight()*3 });
 
-
-        $('.'+_el.body).mousewheel(function(event, delta, deltaX, deltaY){
-            
+        // mousewheel to scroll
+        //--------------------------------------------------------------------------------------------------
+        $('.'+_el.top).mousewheel(function(event, delta, deltaX, deltaY){
+            var next = _page.scrollLeft - delta*40;
+            next = (next < 0)?0:next;
+            window.scrollTo(next, 0);
+            $('.'+_el.h1).css({ 'text-indent' : document.body.scrollLeft/1.4 });//adding this in mousewheel event handler while it is already in scroll's makes it more smooth-like.
+            //$('.'+_el.h1).animate({textIndent:document.body.scrollLeft/1.4}, {duration:50} );//animation only makes it lag, not smoothy.
         });
+        $(window).scroll(function(){
+            $('.'+_el.h1).css({ 'text-indent' : document.body.scrollLeft/1.4 });
+            _page.scrollLeft = document.body.scrollLeft;
+        })
 
-        // Tiles click&hover effects
-        var tiles = $('.'+_el.content);
-        $.each(tiles, function(i, e){
+        
+        //Binding Events
+        //-----------------------------------------------------------------------------------------------------
+        $.each($('.'+_el.content), function(i, e){
             var el = $(this);
-            var duration = 100;
-            el.data('metro',{
+
+            // for a better antialiasing
+            if(el.css('box-shadow')=='none') el.css({ 'box-shadow' : '0 0 1px transparent' });
+            el.parent().css({ '-webkit-perspective' : el.outerHeight()*20 });
+            el.parent().css({ '-o-perspective' : el.outerHeight()*20 });
+            el.parent().css({ '-moz-perspective' : el.outerHeight()*20 });
+            el.parent().css({ '-ms-perspective' : el.outerHeight()*20 });
+
+            // Interaction effects
+            var interaction = {
                 clicking:   false,
                 origin:     0,
                 ang:        '',
                 orizorvert: 0,
-                slide: 0
-            });
+                class:      ''
+            }
+            el.data('interaction', interaction);
+            _i.press(el);
 
-            /* for a better antialiasing */
+            // Presentation effects
+            var presentation = {
+                mode: 'static',
+                manual: false,//Given that now automaton.unserve is unsupported, you can set p.manual to true to override automaton's control right
+                stat: 'initial',
+                trigger: el.attr('data-trigger')||'auto'
+            }
 
-            if(el.css('box-shadow')=='none') el.css({'box-shadow':'0 0 1px transparent'});
-            el.parent().css({'-webkit-perspective':el.outerHeight()*20});
-            el.parent().css({'-o-perspective':el.outerHeight()*20});
-            el.parent().css({'-moz-perspective':el.outerHeight()*20});
-            el.parent().css({'-ms-perspective':el.outerHeight()*20});
+            if(el.hasClass('backface') && !el.hasClass('fragment')){
+                presentation.mode = 'backface';
+                el.data('presentation', presentation);
+                _p.backface(el);
+            } else if(el.hasClass('extend')){
+                presentation.mode = 'extend';
+                presentation.dir = el.attr('data-dir')||'top';
+                el.data('presentation', presentation);
+                _p.extend(el);
+            } else if(el.hasClass('shatter')){
+                presentation.mode = 'shatter';
+                el.data('presentation', presentation);
+                _p.shatter(el);
+            } else if(el.hasClass('tag')){
+                presentation.mode = 'tag';
+                presentation.dir = el.attr('data-dir')||'bottom';
+                el.data('presentation', presentation);
+                _p.tag(el);
+            }
+        })
+    }
 
-            el.mousedown(function(e){
+    $.metro = {};
+
+    //Interaction Effects
+    //-----------------------------------------------------------------------------------------------------
+    var _i = $.metro.interaction = {
+        press: function(el){
+            el.bind('mousedown._i', function(e){
 
                 var mouse = {
                     x:e.pageX-el.offset().left,
                     y:e.pageY-el.offset().top
                 },
 
-                metro=$(this).data('metro');
-                metro.clicking=true;
+                interaction=$(this).data('interaction');
+                interaction.clicking=true;
 
                 if( mouse.x < el.outerWidth()/3 ){
-                    metro.orizorvert = 1;
-                    metro.origin = 100;
-                    metro.ang = 'Neg';
+                    interaction.orizorvert = 1;
+                    interaction.origin = 100;
+                    interaction.ang = 'Neg';
                     /* left */
                 }else if(mouse.x > parseInt(el.outerWidth()*2/3)){
-                    metro.orizorvert = 1;
-                    metro.ang = 'Pos';
+                    interaction.orizorvert = 1;
+                    interaction.ang = 'Pos';
                     /* right */
                 }else{
                     if(mouse.y < el.outerHeight()/3){
-                        metro.orizorvert = 2;
-                        metro.origin = 100;
-                        metro.ang = 'Pos';
+                        interaction.orizorvert = 2;
+                        interaction.origin = 100;
+                        interaction.ang = 'Pos';
                         /* top */
                     }else if(mouse.y > parseInt(el.outerHeight()*2/3)){
-                        metro.orizorvert = 2;
-                        metro.ang = 'Neg';
+                        interaction.orizorvert = 2;
+                        interaction.ang = 'Neg';
                         /* bottom */
                     }
                 }
-                el.data('metro',metro)
 
-                if( metro.orizorvert > 0 && $.browser.webkit){
-                    var anim = 'press'+ metro.ang + (metro.orizorvert==1 ? 'Y':'X');
+                if( interaction.orizorvert > 0 && $.browser.webkit){
+                    var anim = 'press' + interaction.ang + (interaction.orizorvert==1 ? 'Y':'X');
                     el
-                        .attr('class', _el.content+ ' ' + anim)
-                        .delay(duration)
-                } else if( metro.orizorvert==0 || !$.browser.webkit ){
+                        .removeClass(interaction.class)
+                        .addClass(anim)
+                } else if( interaction.orizorvert==0 || !$.browser.webkit ){
                     var anim = 'press';
                     el
-                        .attr('class', _el.content+ ' ' + anim)
-                        .delay(duration);
+                        .removeClass(interaction.class)
+                        .addClass(anim)
                 }
-            }).mouseup(function(e){
-                var a = el.data('metro');
+
+                interaction.class = anim;
+                el.data('interaction',interaction)
+            }).bind('mouseup._i', function(e){
+                var a = el.data('interaction');
 
                 if( a.clicking==true ){
                     if( a.orizorvert > 0 && $.browser.webkit){
-                        var anim = 'pressed'+ a.ang + (a.orizorvert==1 ? 'Y':'X');
+                        var anim = 'pressed' + a.ang + (a.orizorvert==1 ? 'Y':'X');
                         el
-                            .attr('class', _el.content+ ' ' + anim)
-                            .delay(duration);
+                            .removeClass(a.class)
+                            .addClass(anim)
                     }else if( a.orizorvert==0 || !$.browser.webkit){
                         var anim = 'pressed';
                         el
-                          .attr('class', _el.content+ ' ' + anim)
-                          .delay(duration);
+                          .removeClass(a.class)
+                          .addClass(anim)
                     }
-                    el.data('metro',{
+                    el.data('interaction',{
                         clicking:   false,
                         origin:     0,
                         ang:        '',
-                        orizorvert: 0
+                        orizorvert: 0,
+                        class:      anim
                     })
                 }
-            }).mouseenter(function(){
-                if (el.hasClass("tile-multi-content")){
-                    var c_main = $(el.children(".tile-content-main"));
-                    var c_sub = $(el.children(".tile-content-sub"));
-                    var subHeight = c_sub.height()+5;
-                    c_main.animate({"marginTop": - subHeight}, 100);
-                    c_sub.css("opacity", 1);
-                }
-            }).mouseleave(function(){
-                if (el.hasClass("tile-multi-content")){
-                    var c_main = $(el.children(".tile-content-main"));
-                    var c_sub = $(el.children(".tile-content-sub"));
-                    var subHeight = c_sub.height();
-                    c_main.animate({"marginTop": 0}, 100);
-                    c_sub.css("opacity", .1);
-                }
-            }).mouseout(function(){
-                if( el.data('metro').clicking ){
+
+                //Turn Page effect
+                //$('.' + $.fn.metro.defaults.el.page).css({ '-webkit-transform-origin' : _page.scrollLeft+'px 0' }).addClass( 'turn' );
+
+            }).bind('mouseout._i', function(){
+                if( el.data('interaction').clicking ){
                    el.mouseup()
                 }
             })
-        })
-
-        /*
-
-        // Selectable
-        var selectables = $(".selectable");
-        $.each(selectables, function(i, e){
-            var el = $(this);
-            var items = el.children(".metro-image, .metro-image-overlay, .metro-icon-text, .metro-image-text");
-            items.bind("click", function(){
-                if ($(this).hasClass("disabled")) return;
-                $(this).toggleClass("selected");
-            })
-        })
-
-        // Metro-Switchers
-        var switchers = $(".metro-switch");
-        switchers.bind("click", function(){
-            var el = $(this);
-            if (el.hasClass('disabled') || el.hasClass('static')) return false;
-            el.toggleClass("state-on");
-        })
-
-        */
+        }
     }
-})(jQuery)
+      
+    //Presentation Effects
+    //-----------------------------------------------------------------------------------------------------
+    var _p = $.metro.presentation = {
+        //Backface Presentation Effect
+        backface: function($el){
+            var triggerName = {
+                trigger: 'backface',
+                triggered: 'backfaced'
+            }
+            var presentation = $el.data('presentation');
+            var flipdir;
+            if($el.find('.back').hasClass('flip-x')){
+                flipdir = 'flip-x';
+            } else if($el.find('.back').hasClass('flip-y')){
+                flipdir = 'flip-y';
+            }
+
+            $el.bind('backface._p', function(){
+                $el.children('.card').addClass(flipdir);
+                presentation.stat = 'triggered';
+                $el.data('presentation', presentation);
+            }).bind('backfaced._p', function(){
+                $el.children('.card').removeClass(flipdir);
+                presentation.stat = 'initial';
+                $el.data('presentation', presentation);
+            })
+
+            this._trigger(presentation, $el, triggerName) 
+
+        },
+        //Extend Presentation Effect
+        extend: function($el){
+            var triggerName = {
+                trigger: 'extend',
+                triggered: 'extended'
+            }
+            var presentation = $el.data('presentation');
+            var dir = presentation.dir;
+            var er = $el.find('.extender');
+            var ee = $el.find('.extendee');
+            var size;
+            if(dir === 'top' || dir=== 'bottom'){
+                size = ee.outerHeight();
+            } else if (dir === 'left'|| dir === 'right'){
+                size = ee.outerWidth();
+            }
+
+
+            $el.bind('extend._p', function(){
+                er.css( 'margin-' + dir , -size+'px' );//wtf???
+                presentation.stat = 'triggered';
+                $el.data('presentation', presentation);
+            }).bind('extended._p', function(){
+                er.css( 'margin-' + dir, '0px' );
+                presentation.stat = 'initial';
+                $el.data('presentation', presentation);
+            })
+
+            this._trigger(presentation, $el, triggerName)
+
+
+        },
+        //Shattered Presentation Effect
+        shatter: function($el){
+            var presentation = $el.data('presentation');
+            var fragments = $el.children('.backface');
+            var fragments_trigger = presentation.trigger === 'free'?'auto':'controlled';
+
+            fragments.data('presentation', {
+                mode: 'backface',
+                manual: false,
+                stat: 'initial',
+                trigger: fragments_trigger
+            });
+            //_p.backface(fragments);
+            fragments.each(function(){
+                _p.backface($(this));
+            })
+
+            if(presentation.trigger === 'hover'){
+                $el.bind('mouseover._p', function(){
+                    fragments.trigger('backface._p');
+                    presentation.stat = 'triggered';
+                    $el.data('presentation', presentation);
+                }).bind('mouseout._p', function(){
+                    fragments.trigger('backfaced._p');
+                    presentation.stat = 'initial';
+                    $el.data('presentation', presentation);
+                })
+            } else if( presentation.trigger === 'free'){
+                //nothing you can do here
+            } else if( presentation.trigger === 'auto'){
+                $.metro.automaton.serve($el);
+                $el.bind('dispatch._p', function(){
+                    var p = $(this).data('presentation');
+                    var stat = p.stat;
+                    if(p.manual === true){ return }
+                    if(stat === 'initial'){
+                        fragments.trigger('backface._p');
+                        p.stat = 'triggered';
+                        $el.data('presentation', p);
+                    } else if( stat === 'triggered' ){
+                        fragments.trigger('backfaced._p');
+                        p.stat = 'initial';
+                        $el.data('presentation', p);
+                    }
+                })
+            }
+        },
+        //Tag Presentation Effect
+        tag: function($el){
+            var triggerName = {
+                trigger: 'tag',
+                triggered: 'tagged'
+            }
+            var presentation = $el.data('presentation');
+            var dir = presentation.dir;
+            var sign = $el.find('.sign');
+            var size;
+            if(dir === 'top' || dir=== 'bottom'){
+                size = sign.outerHeight();
+            } else if (dir === 'left'|| dir === 'right'){
+                size = sign.outerWidth();
+            }
+
+
+            $el.bind('tag._p', function(){
+                sign.css( dir , '0px' );
+                presentation.stat = 'triggered';
+                $el.data('presentation', presentation);
+            }).bind('tagged._p', function(){
+                sign.css( dir, -size + 'px' );
+                presentation.stat = 'initial';
+                $el.data('presentation', presentation);
+            })
+
+            this._trigger(presentation, $el, triggerName);
+            
+        },
+        _trigger: function(data, $el, name){
+            if(data.trigger === 'hover'){
+                $el.bind('mouseover._p', function(){
+                    $el.trigger(name.trigger + '._p');
+                }).bind('mouseout._p', function(){
+                    $el.trigger(name.triggered + '._p');
+                })
+            } else if( data.trigger === 'auto'){
+                $.metro.automaton.serve($el);
+                $el.bind('dispatch._p', function(){
+                    var p = $el.data('presentation');
+                    var stat = p.stat;
+                    if(p.manual === true){ return }
+                    if(stat === 'initial'){
+                        $el.trigger(name.trigger + '._p');
+                    } else if( stat === 'triggered' ){
+                        $el.trigger(name.triggered + '._p');
+                    }
+                })
+            }
+        }
+    }
+
+    //Automaton
+    //-----------------------------------------------------------------------------------------------------
+    $.metro.automaton = (function(opt){
+        var els = [];
+        return {
+            opt: opt,
+            serve: function(el){
+                els.push(el);
+            },
+            unserve: function(el){
+                //no effective way yet
+            },
+            dispatch: function(parallel){
+                var rand = Math.random();
+                var l = els.length;
+                if(parallel > l){
+                    parallel = l;
+                }
+                if(parallel === 0){ return }
+                var nth = Math.floor(rand*l);
+                els[nth].trigger('dispatch._p');
+            },
+            _init: function(opt){
+                setInterval("$.metro.automaton.dispatch($.metro.automaton.opt.parallel)", $.metro.automaton.opt.density);
+            }
+        }
+    })({
+        density: 1500,
+        parallel: 1,
+        delay: 2000
+    })
+    //Automaton need to init at a global scope after it is defined.
+    setTimeout("$.metro.automaton._init($.metro.automaton.opt)", $.metro.automaton.opt.delay)
+
+    $.fn.metro.defaults = {
+        el:{
+            top: 'metro',
+            page : 'metro-wrapper',
+            h1 : 'metro-h1',
+            body : 'metro-body',
+            section : 'metro-section',
+            content : 'metro-content'
+        }
+    }
+})(jQuery, window)
